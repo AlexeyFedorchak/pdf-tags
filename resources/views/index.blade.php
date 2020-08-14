212ae0a15fe3e52e21f80115a383f1e53916bb60
Oleksii Fedorchak <ofedorchak68@gmail.com>
2020-08-14 18:48:44 +0300

    Update: added route and blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+Form should be here..
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/pdf', function () {
-    dd(1);
-});
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
